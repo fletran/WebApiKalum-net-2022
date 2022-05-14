a70bd4c8e72a122e0181da8b803c08aaa83177cb
fletran <teletran.system@gmail.com>
2022-05-14 12:10:37 -0600

    Enlazar Modelo con clase Carrera Tecnica con EF

---
+++ KalumDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiKalum.Entities;
+namespace WebApiKalum
+{
+    public class KalumDbContext : DbContext
+    {
+        public DbSet<CarreraTecnica> CarreraTecnica {get;set;}
+        public KalumDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CarreraTecnica>().ToTable("CarreraTecnica").HasKey(ct => new {ct.CarreraId});
+        }
+    }
+}
