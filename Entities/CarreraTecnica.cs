410c534c24fd02aa050dc01abf8829d42815ce9e
fletran <teletran.system@gmail.com>
2022-06-11 11:01:02 -0600

    Finalizacion del modulo de Carreras Tecnicas

--- Entities/CarreraTecnica.cs
+++ Entities/CarreraTecnica.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApiKalum.Entities
 {
     public class CarreraTecnica
     {
+        [Required (ErrorMessage = "El campo {0} es Requerido")]
         public string CarreraId {get;set;}
-
+        [Required (ErrorMessage = "El campo {0} es Requerido")]
+        [StringLength(128,MinimumLength = 5, ErrorMessage = "La cantidad minima de cracteres es {2} y la maxima es {1} para el campo {0} ")]
         public string Nombre {get;set;}
 
         public virtual List<Aspirante> Aspirantes {get; set;}
